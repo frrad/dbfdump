4488172a0732f292310a7d8e3823f3f79d3dbd40
Fred Robinson <frederick.robinson@gmail.com>
2019-05-27 22:48:25 -0700

    works

--- main.go
+++ main.go
@@ -7,19 +7,23 @@ import (
 
 	"github.com/SebastiaanKlippert/go-foxpro-dbf"
 	_ "github.com/mattn/go-sqlite3"
+
+	"gopkg.in/cheggaaa/pb.v1"
 )
 
 func main() {
+	tableName := "people"
 	sourceFile := "/home/frederickrobinson/go/src/github.com/frrad/meckleen/parcel_taxdata/Parcel_TaxData.dbf"
-	colNames, colTypes, rows := readDbf(sourceFile)
+	outPath := "/home/frederickrobinson/outfile.sqlite"
+
+	colNames, colTypes, rows := readDbf(sourceFile, 0)
 
 	sqliteTypes, err := sqliteColumnsFromDBF(colTypes)
 	if err != nil {
 		log.Fatalf("problem translating sqlite column types: %v", err)
 	}
 
-	outPath := "/home/frederickrobinson/outfile.sqlite"
-	err = writeSQLite(outPath, colNames, sqliteTypes, rows)
+	err = writeSQLite(tableName, outPath, colNames, sqliteTypes, rows)
 	if err != nil {
 		log.Fatalf("problem writing outdb: %v", err)
 	}
@@ -45,7 +49,22 @@ func sqliteColumnsFromDBF(colTypes []byte) ([]string, error) {
 	return out, nil
 }
 
-func buildCreate(colNames, colTypes []string) (string, error) {
+func buildInsert(tableName string, colNames []string) (string, error) {
+	n := len(colNames)
+	nameStr, placeholderStr := "", ""
+
+	for i := 0; i < n-1; i++ {
+		nameStr += fmt.Sprintf("%s, ", colNames[i])
+		placeholderStr += "?, "
+	}
+	nameStr += fmt.Sprintf("%s", colNames[n-1])
+	placeholderStr += "?"
+
+	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)",
+		tableName, nameStr, placeholderStr), nil
+}
+
+func buildCreate(tableName string, colNames, colTypes []string) (string, error) {
 	n := len(colNames)
 	if len(colTypes) != n {
 		return "", fmt.Errorf("found %d column names but %d types", len(colNames), len(colTypes))
@@ -57,40 +76,49 @@ func buildCreate(colNames, colTypes []string) (string, error) {
 	}
 	ans += fmt.Sprintf("%s %s", colNames[n-1], colTypes[n-1])
 
-	return fmt.Sprintf("CREATE TABLE people (%s)", ans), nil
+	return fmt.Sprintf("CREATE TABLE %s (%s)", tableName, ans), nil
 }
 
-func writeSQLite(outPath string, colNames []string, colTypes []string, rows <-chan []interface{}) error {
+func writeSQLite(tableName, outPath string, colNames []string, colTypes []string, rows <-chan []interface{}) error {
 	database, err := sql.Open("sqlite3", outPath)
 	if err != nil {
 		return err
 	}
 	log.Println("opened db", outPath)
 
-	createString, err := buildCreate(colNames, colTypes)
+	createString, err := buildCreate(tableName, colNames, colTypes)
 	if err != nil {
 		return err
 	}
-	log.Printf("creating table with statement '%s'\n", createString)
 	_, err = database.Exec(createString)
 	if err != nil {
 		return err
 	}
 
-	insertStmt, err := database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	insertString, err := buildInsert(tableName, colNames)
 	if err != nil {
 		return err
+	}
+	insertStmt, err := database.Prepare(insertString)
+	if err != nil {
+		return fmt.Errorf("trouble with %s: %+v", insertString, err)
 	}
 	defer func() {
 		err := insertStmt.Close()
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 		}
 	}()
-	_, err = insertStmt.Exec("Nic", "Raboy")
-	if err != nil {
-		return err
+
+	_, err = database.Exec("BEGIN TRANSACTION")
+	for row := range rows {
+		_, err = insertStmt.Exec(row...)
+		if err != nil {
+			return err
+		}
 	}
+	_, err = database.Exec("END TRANSACTION")
+
 	return nil
 }
 
@@ -103,7 +131,7 @@ func columnTypes(headers []dbf.FieldHeader) []byte {
 	return ans
 }
 
-func readDbf(filename string) ([]string, []byte, <-chan []interface{}) {
+func readDbf(filename string, maxOver uint32) ([]string, []byte, <-chan []interface{}) {
 	hax := func(version byte) error {
 		if version == 0x03 {
 			return nil
@@ -121,14 +149,22 @@ func readDbf(filename string) ([]string, []byte, <-chan []interface{}) {
 	headerTypes := columnTypes(testdbf.Fields())
 	headerNames := testdbf.FieldNames()
 
-	rowChan := make(chan []interface{}, 1<<10)
+	rowChan := make(chan []interface{}, 10)
 
 	go func(x *dbf.DBF, output chan<- []interface{}) {
 		defer testdbf.Close()
 		defer close(output)
 
 		max := x.NumRecords()
+		if maxOver > 0 {
+			max = maxOver
+		}
+
+		bar := pb.StartNew(int(max))
+
 		for i := uint32(0); i < max; i++ {
+			bar.Increment()
+
 			r, err := x.RecordAt(i)
 			if err != nil {
 				continue
@@ -137,6 +173,7 @@ func readDbf(filename string) ([]string, []byte, <-chan []interface{}) {
 				output <- r.FieldSlice()
 			}
 		}
+		bar.Finish()
 
 	}(testdbf, rowChan)
 
